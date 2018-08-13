c33156f0a5e69b9de5bf0af389206826f3d0798b
SCratORS <scrators@mail.ru>
2018-08-13 21:23:33 +0300

    Update analogFastWrite.ino

--- analogFastWrite.ino
+++ analogFastWrite.ino
@@ -5,7 +5,7 @@ void fCOM0B1 (uint8_t val) {DDRD |= 0x40; if (val == 0x00) PORTD &= ~0x40; else 
 void fCOM1A1 (uint8_t val) {DDRB |= 0x02; if (val == 0x00) PORTB &= ~0x02; else if (val == 0xFF) PORTB |= 0x02; else {sbi(TCCR1A, COM1A1); OCR1A = val;}}//D9
 void fCOM1B1 (uint8_t val) {DDRB |= 0x04; if (val == 0x00) PORTB &= ~0x04; else if (val == 0xFF) PORTB |= 0x04; else {sbi(TCCR1A, COM1B1); OCR1B = val;}}//D10
 void fCOM2A1 (uint8_t val) {DDRB |= 0x08; if (val == 0x00) PORTB &= ~0x08; else if (val == 0xFF) PORTB |= 0x08; else {sbi(TCCR2A, COM2A1); OCR2A = val;}}//D11
-void (*analogFastWrite[])(uint8_t) = {&fCOM2B1,&fCOM0A1,&fCOM2B1,&fCOM1A1,&fCOM1B1,&fCOM2A1};
+void (*analogFastWrite[])(uint8_t) = {&fCOM2B1,&fCOM0A1,&fCOM0B1,&fCOM1A1,&fCOM1B1,&fCOM2A1};
 
 void setup() {
   // put your setup code here, to run once:
